cc059bd26e787f6c935fea1628c95ca244824fad
Abdel <abdelhalim.ria@gmail.com>
2025-07-31 16:35:10 -0400

    import from d3 package instead of subs

--- ui/user/src/lib/components/graph/LineGraph.svelte
+++ ui/user/src/lib/components/graph/LineGraph.svelte
@@ -1,6 +1,6 @@
 <script lang="ts" generics="T">
 	import { Chart, Axis, Spline, Tooltip, Highlight, Svg } from 'layerchart';
-	import { scaleTime } from 'd3-scale';
+	import { scaleTime } from 'd3';
 	import { formatTime } from '$lib/time';
 	import { onMount } from 'svelte';
 
